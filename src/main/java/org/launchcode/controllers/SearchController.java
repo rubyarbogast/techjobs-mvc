605c7c6e62828575a6221e96f11b0dae633b0cff
Ruby Arbogast <ruby.arbogast@gmail.com>
2017-08-19 12:37:18 -0700

    Implement search functionality

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,10 +23,42 @@ public class SearchController {
     }
 
     /** TODO #1 - Create handler to process search request and display results
+     * should take 2 parameters: type of search and search term
      * features must be named based on corresponding form field names
      * use the correct annotations for the method and parameters
      * refer to the form action in search.html to configure the correct mapping route
      * after looking up search results via JobData class, pass them into search.html view via the model
      * pass ListController.columnChoices to the view, as existing search handler does
+     * say number of results
      */
+    //TODO:
+    // RequestMapping value should be search/results
+    // search term is searchTerm; type is searchType
+    // method should be get
+    // use if/else -- if the search is in all columns (findByValue), else if search is in a specific column (findByColumnAndValue)
+
+    //searchType says which column to search
+
+    @RequestMapping(value="results")
+    public String search(Model model,
+                         @RequestParam String searchTerm, @RequestParam String searchType) {
+        // look up the search results via the JobData class -- use findByValue
+        ArrayList<HashMap<String, String>> jobs;
+        //if searchType is all, use JobData.findByValue
+        //else, use findByColumnAndValue
+        //is "all" the right thing to use?
+        if(searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        // pass to search view:
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+
+    }
+
 }
