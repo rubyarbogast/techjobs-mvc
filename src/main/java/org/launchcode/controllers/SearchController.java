e99ac17d5273f5ccdea96d167709d401fd10c231
Ruby Arbogast <ruby.arbogast@gmail.com>
2017-08-27 11:39:04 -0700

    Added page headings with number of search results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -25,40 +25,24 @@ public class SearchController {
         return "search";
     }
 
-    /** TODO #1 - Create handler to process search request and display results
-     * should take 2 parameters: type of search and search term
-     * features must be named based on corresponding form field names
-     * use the correct annotations for the method and parameters
-     * refer to the form action in search.html to configure the correct mapping route
-     * after looking up search results via JobData class, pass them into search.html view via the model
-     * pass ListController.columnChoices to the view, as existing search handler does
-     * say number of results
-     */
-    //TODO:
-    // RequestMapping value should be search/results
-    // search term is searchTerm; type is searchType
-    // method should be get
-    // use if/else -- if the search is in all columns (findByValue), else if search is in a specific column (findByColumnAndValue)
-
-    //searchType says which column to search
-
     @RequestMapping(value="results")
     public String search(Model model,
                          @RequestParam String searchTerm, @RequestParam String searchType) {
-        // look up the search results via the JobData class -- use findByValue
         ArrayList<HashMap<String, String>> jobs;
 
+        // if the search is in all columns, look up results via findByValue,
+        // else search is in a specific column, use findByColumnAndValue
+        // either way, add the jobs found to the ArrayList of jobs
         if(searchType.equals("all")) {
             jobs = JobData.findByValue(searchTerm);
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
 
-        System.out.println(searchType);
-
-        // pass to search view:
+        // pass search results found above to search view:
         model.addAttribute("columns", ListController.columnChoices);
         model.addAttribute("jobs", jobs);
+        // pass searchType back to the view so that the user's column selection will stay selected
         model.addAttribute("searchType", searchType);
         return "search";
 
